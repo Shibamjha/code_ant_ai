2af495b4aafe8db4d926ebef117760bba5712707
Abhishek Jha <jhaa213152@gmail.com>
2024-12-21 19:51:43 +0530

    OK

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div className="text-red-500">Home</div>;
+  return (
+    <div className="text-red-500">
+      Home
+    </div>
+  );
 };
 
 export default Home;
